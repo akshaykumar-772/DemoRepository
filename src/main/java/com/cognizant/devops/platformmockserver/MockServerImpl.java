8e0f05ed88ecea112a34a9131fa91e1481b402af
Akshay Kumar <51229993+akshaykumar-772@users.noreply.github.com>
2019-09-20 15:33:24 +0530

    Commented Map import

--- src/main/java/com/cognizant/devops/platformmockserver/MockServerImpl.java
+++ src/main/java/com/cognizant/devops/platformmockserver/MockServerImpl.java
@@ -27,7 +27,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
